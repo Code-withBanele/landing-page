f8986df9cb50784451805f8460a71505acb48d58
Code-withBanele <st10489580@rcconnect.edu.za>
2025-09-04 00:13:48 +0200

    Refactor code structure for improved readability and maintainability changed bg on all pages, fixed layout issues on most page started JS file updated style sheets to suit new bg, added date feature on start-a-page.html

--- JS_Assets/script.js
+++ JS_Assets/script.js
@@ -1,15 +1,59 @@
-const radios = document.querySelectorAll('input[name= "item"]');
-const selectedItem = document.getElementById('selected-Item');
-const selectedPrice = document.getElementById('selected-price');
-const QuoteButton = document.getElementById('Qbutton');
+const slipDate = document.getElementById('slip-date');
+const slipItems = document.getElementById('slip-items');
+const slipTotal = document.getElementById('slip-total');
 
 
-//update slip
+function updateSlip(){
+    slipDate.textcontent = "Date: " + new Date().toLocaleDateString();
+    
+    
+    const selected = document.querySelectorAll("input[type = 'checkbox' ] : checked");
+    let items = [];
+    let total = 0;
+    let discount = false;
 
-radios.forEach(radios => {
-    radios.addEventListener('change', () =>{
-        selectedItem.textContent = 'Item: ${radio.value}';
-        selectedPrice.textContent = 'Price: R${radio.dataset.price}';
+    slipItems.innerHTML = "";
 
+    selected.forEach(input => {
+        const name = input.dataset.name;
+        const price = input.dataset.price;
+
+        if (price === "discount"){
+            discount = true;    
+        }else{
+            const value = parseFloat(price);
+            total += value;
+            items.push({ name, value});
+        }
     });
+
+    if(discount){
+        slipTotal*0.10;
+        items.push({name: "10% discoun applied", value: -0.1});
+    }
+
+    items.forEach(item =>{
+        const li = document.createElement("li");
+        li.textContent = '${item.name} - R${item.value}';
+        slipItems.appendChild(li);
+    });
+
+    slipTotal.textContent = "R" + total.toFixed(2);
+
+} 
+
+document.querySelector("input[type='checkboxes']").forEach(input => {
+    input.addEventListner("change", updateSlip);
 });
+
+setInterval(updateSlip, 1000);
+
+document.getElementById("QButton").addEventListener("click", ()=>{
+    const body = "Here is your project quote: n\n" + slipItems.innerText + "\n\nTotal Cost: " + slipTotal.innerText;
+    window.location.href = "mailtoyou@example.com?"
+});
+
+document.getElementById("MButton").addEventListener("click", () => {
+    window.location.href ="#booking-calendar-link";
+});
+
